d1eeadfcae95ea782fe75884d322807e917078db
Aishwarya Dev Singh <aishwaryadevsingh9@gmail.com>
2025-12-28 14:30:12 +0530

    Reformat app.py to fix import alignment

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
+
 st.title('🏠House Price prediction using ML')
+
 
